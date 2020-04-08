f9f10f159f75cbefa4b772d95903796d4a3a874a
Victor Nevola <victornevolapn@gmail.com>
2020-04-08 19:31:47 -0300

    Registro de usuario

--- src/App.js
+++ src/App.js
@@ -1,37 +1,6 @@
 import React, { Component } from 'react'
-import firebase from './fireConnection';
 import Routes from './routes';
 export default class App extends Component {
-
-  state = {
-    email: '',
-    senha: '',
-  }
-
-
-  cadastrar = (e) => {
-
-    //Cadastrando um usuario
-    firebase.auth().createUserWithEmailAndPassword(this.state.email, this.state.senha)
-      .then((succes) => {
-        alert('cadastrado com sucesso');
-      })
-      .catch((err) => {
-        if (err.code === 'auth/invalid-email') {
-          alert('Email Invalido');
-        };
-        if (err.code === 'auth/weak-password') {
-          alert('Senha Fraca');
-        } else {
-          alert(err.code)
-        }
-      });
-
-
-    e.preventDefault();
-  }
-
-
 
   render() {
     return (
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,10 +1,68 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import firebase from '../fireConnection';
+import Header from '../components/Header';
 
 export default class Register extends Component {
+    state = {
+        email: '',
+        password: '',
+        message: ''
+    }
+
+
+    handlerInputs = (e) => {
+        let state = this.state;
+        state[e.target.name] = e.target.value;
+        this.setState(state);
+    }
+
+    register = (e) => {
+        e.preventDefault();
+        firebase.auth().createUserWithEmailAndPassword(this.state.email, this.state.password)
+            .then((succes) => {
+                this.messageAuth('Cadastrado com sucesso');
+            })
+            .catch((err) => {
+                if (err.code === 'auth/invalid-email') {
+                    this.messageAuth('Email Invalido');
+                };
+                if (err.code === 'auth/weak-password') {
+                    this.messageAuth('Senha Fraca');
+                } else {
+                    alert(err.code)
+                }
+            });
+    };
+
+    messageAuth = (message) => {
+        this.setState({ message: message });
+        setTimeout(() => {
+            this.setState({ message: '' })
+        }, 15000);
+    }
+
     render() {
         return (
             <div>
-                <h2>cadastrar</h2>
+                <Header way={
+                    [
+                        { way: '/', nameWay: "Home" },
+                        { way: 'sing-up', nameWay: 'Login' }
+                    ]
+                } />
+                <h1>Tela de Cadastro de usuario</h1>
+                <form onSubmit={this.register}>
+                    <label>Email:</label>
+                    <input type="Email" name="email" placeholder="Email" onChange={this.handlerInputs} value={this.state.email} />
+                    <br />
+                    <label>Senha:</label>
+                    <input type="password" name='password' placeholder="Email" onChange={this.handlerInputs} value={this.state.password} />
+                    <br />
+                    <button type='submit'>Cadastrar</button>
+                </form>
+                {
+                    this.state.message !== '' && <h2>{this.state.message}</h2>
+                }
             </div>
         )
     }
